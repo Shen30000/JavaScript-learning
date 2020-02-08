32a348d8c139a55322aab4b99db0f004d0cd023b
Shen30000 <188976656@qq.com>
2020-02-08 16:51:13 +0800

    update chapter2

--- chapter2/battleship.js
+++ chapter2/battleship.js
@@ -1,6 +1,7 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 var guess;
 var hits = 0;
 var guesses = 0;
@@ -12,13 +13,15 @@ while (isSunk == false) {
         alert("Please enter a valid cell number!");
     } else {
         guesses = guesses + 1;
+
         if (guess == location1 || guess == location2 || guess == location3) {
             hits = hits + 1;
-            alert("HIT!")
+            alert("HIT!");
             if (hits == 3) {
                 isSunk = true;
-                alert("You sank my battleship!")
+                alert("You sank my battleship!");
             }
+
         } else {
             alert("MISS");
         }
